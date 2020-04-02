62b5b81c59aed321d059b33dbaa30ed27a33a317
daif <daif@deepblueai.com>
2020-04-02 16:43:16 +0800

    12

--- frameWidget/src/main/java/com/mdx/framework/service/subscriber/HttpResultSubscriberListener.kt
+++ frameWidget/src/main/java/com/mdx/framework/service/subscriber/HttpResultSubscriberListener.kt
@@ -1,6 +1,6 @@
 package com.mdx.framework.service.subscriber
 
 interface HttpResultSubscriberListener {
-    fun onSuccess(data: String?, method: String)
+    fun onSuccess(data: String?, method: String): Boolean
     fun onError(code: String?, msg: String?, data: String?, method: String)
 }
